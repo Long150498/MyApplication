1503ed4760afc8cf5070e6393dbf4ec78b1c02e7
Nguyễn Thành Long <longnt6@vnpay.vn>
2023-10-14 14:22:05 +0700

    set image

--- app/src/main/java/com/example/myapplication/DetailImageActivity.kt
+++ app/src/main/java/com/example/myapplication/DetailImageActivity.kt
@@ -22,10 +22,43 @@ class DetailImageActivity : AppCompatActivity() {
             uri = Uri.parse(intent.getStringExtra("URI"))
         }
         image = findViewById<TouchImageView>(R.id.ivDetail)
-        val f = File(uri?.path)
-        val bmOptions = BitmapFactory.Options()
-        var bitmap = BitmapFactory.decodeFile(f.path, bmOptions)
-        bitmap = Bitmap.createScaledBitmap(bitmap!!, 100, 100, true)
+        val bitmap = decodeSampledBitmapFromFile(uri.toString(),500,500)
         image?.setImageBitmap(bitmap)
+    }
+
+    private fun decodeSampledBitmapFromFile(
+        filePath: String,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(filePath, options)
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+
+        options.inJustDecodeBounds = false
+        return BitmapFactory.decodeFile(filePath, options)
+    }
+
+    private fun calculateInSampleSize(
+        options: BitmapFactory.Options,
+        reqWidth: Int,
+        reqHeight: Int
+    ): Int {
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
     }
 }
